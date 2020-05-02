074c7fe0efb14d4ab99b20cc6d0fec9ba53fab84
jana-choi <lovej0918@gmail.com>
2020-05-02 18:23:39 +0900

    Chapter 08

---
+++ Chapter 08/getter_setter.py
@@ -0,0 +1,36 @@
+import math
+
+class Circle:
+    def __init__(self, radius):
+        self.__radius = radius
+    
+    def get_circumference(self):
+        return 2 * math.pi * self.__radius
+    
+    def get_area(self):
+        return math.pi * (self.__radius ** 2)
+
+    def get_radius(self):
+        return self.__radius
+    
+    def set_radius(self, value):
+        if value <= 0:
+            raise TypeError("길이는 양의 숫자여야 합니다.")
+        self.__radius = value
+
+
+circle = Circle(10)
+
+print("# 원의 둘레와 넓이를 구합니다.")
+print("원의 둘레:", circle.get_circumference())
+print("원의 넓이:", circle.get_area())
+print()
+
+print("# __radius 에 접근합니다.")
+print(circle.get_radius())
+print()
+
+circle.set_radius(-2)
+print("# 반지름을 변경하고 원의 둘레와 넓이를 구합니다.")
+print("원의 둘레:", circle.get_circumference())
+print("원의 넓이:", circle.get_area())
